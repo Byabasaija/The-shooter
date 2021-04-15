041cfd84207a9b7216e47ff397149967835932d4
Byabasaija <basaijapascal9@gmail.com>
2021-04-15 23:15:14 +0300

    Add Scenes and minimum phaser game template

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import Phaser from 'phaser';
+
+import config from './config/config';
+import BootScene from './scenes/BootScene';
+import PreloaderScene from './scenes/PreloaderScene';
+import TitleScene from './scenes/TitleScene';
+import GameScene from './scenes/GameScene';
+
+
+class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Game', GameScene);
+    this.scene.start('Game');
+  }
+}
+
+window.game = new Game();
